a643c9b9f812a242b3c0d7eee673ab4861234a78
jbarker <jackson.barker@du.edu>
2021-12-04 14:06:42 -0700

    upvote model and comment/upvote index model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Upvotes = require('./Upvotes')
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -10,4 +11,38 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Upvotes.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Upvotes.belongsTo(Blog, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Upvotes, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+
+module.exports = { User, Blog, Comment, Upvotes };
