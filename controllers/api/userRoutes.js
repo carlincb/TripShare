2dae94abb319f6ba603737d41d2916ff1e273ea0
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-04 12:57:41 -0700

    comment route added. Starting script for adding comments on the front end.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+      const userData = await User.findAll({
+          include: [Comment, Blog]
+      });
+      res.status(200).json(userData);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+      const userData = await User.findOne({
+          where: {
+              id: req.params.id
+          }
+      });
+      res.status(200).json(userData);
+  } catch (err) {
+      res.status(404).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
