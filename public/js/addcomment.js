8c80d883a45e3218f0e624edb8cf60d87630653c
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-07 10:04:06 -0700

    comments now have an id for the blog.

--- public/js/addcomment.js
+++ public/js/addcomment.js
@@ -4,11 +4,14 @@ var commentText;
 commentBtn.click(function(){
     commentText = $(this).siblings('textarea').val();
     console.log(commentText);
+    blogId = $(this).parents('.blog').attr('blog-id');
+
     $.ajax({
         url: window.location.href + 'api/comments',
         dataType: 'json',
         type: 'POST',
         data: {
+            "post_id": parseInt(blogId),
             "commentContent": commentText,
             "date_created": new Date()
     },
