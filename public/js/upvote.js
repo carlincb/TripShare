e4eac1865f66a21c1c086decd92e44ca641e0831
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-10 12:27:23 -0700

    Working on sending the upvote data to the server.

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -5,23 +5,24 @@ let likecontainer = document.getElementById('likecontainer')
 
 likecontainer.addEventListener('click',  function (event) {
   event.preventDefault();
-  if (event.target.className === "like-or-dislike-btn" || event.target.matches("i")){
+  if (event.target.className === "like-btn" || event.target.matches("i")){
     const El = event.target.matches("button") ? event.target.nextSibling.nextSibling: event.target.parentElement.nextSibling.nextSibling
     console.log(El);
     let likecount = parseInt(El.textContent);
     likecount++;
     El.textContent = likecount;
 
-
+    $.ajax({
+      url: window.location.href + 'api/upvotes',
+      type: 'PUT',
+      dataType: 'json',
+      data: {
+        "upvotes": likecount
+      }
+    });
   }
 
-  $.ajax({
-    type: "PUT",
-    dataType: "json",
-    data: {
-      
-    }
-  })
+
 });
 
 
