b98929e9d754332b0d447f500d0f94fa2b0cb336
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-11 12:12:33 -0700

    Adding functions to make the code more readable.

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -2,13 +2,12 @@ var likeBtn = $('.like-btn');
 var dislikeBtn = $('.dislike-btn');
 
 likeBtn.click(function(){
-    likeSpot = $(this).siblings('.likecount').text()
-    likeCount = parseInt(likeSpot);
-    dislikeSpot = $(this).siblings('.dislikecount').text();
-    dislikeCount = parseInt(dislikeSpot);
-    blogId = $(this).parents('.blog').attr('blog-id');
+    VariableDeclare($(this))
+    console.log(likeSpot)
+    console.log(blogId)
 
     $.get(window.location.href + 'api/upvotes', function(data){
+        console.log(data);
         data.forEach(upvote => {
             if (upvote.user_id == $('nav label').attr('user-id') && upvote.blog_id == blogId) {
                 statementExecuted = true;
@@ -31,37 +30,58 @@ likeBtn.click(function(){
                 }
             }
         });
-    })
+
+        existChecker(statementExecuted);
+    });
 });
 
 dislikeBtn.click(function(){
-    likeSpot = $(this).siblings('.likecount').text()
-    likeCount = parseInt(likeSpot);
-    dislikeSpot = $(this).siblings('.dislikecount').text();
-    dislikeCount = parseInt(dislikeSpot);
-    blogId = $(this).parents('.blog').attr('blog-id');
+    VariableDeclare($(this));
 
     $.get(window.location.href + 'api/upvotes', function(data){
         console.log(data);
-        if (upvote.user_id == $('nav label').attr('user-id') && upvote.blog_id == blogId) {
-            statementExecuted = true;
-            if (!upvote.upvotes && !upvote.downvotes) {
-                upvote.downvotes = true;
-                dislikeCount++;
+        data.forEach(upvote => {
+            if (upvote.user_id == $('nav label').attr('user-id') && upvote.blog_id == blogId) {
+                statementExecuted = true;
+                if (!upvote.upvotes && !upvote.downvotes) {
+                    upvote.downvotes = true;
+                    dislikeCount++;
+                }
+                else if (upvote.upvotes && !upvote.downvotes) {
+                    upvote.upvotes = false;
+                    upvote.downvotes = true;
+                    likeCount--;
+                    dislikeCount++;
+                }
+                else if (!upvote.upvotes && upvote.downvotes) {
+                    upvote.downvotes = false;
+                    dislikeCount--;
+                }
+                else {
+                    console.log('There was an error');
+                }
             }
-            else if (upvote.upvotes && !upvote.downvotes) {
-                upvote.upvotes = false;
-                upvote.downvotes = true;
-                likeCount--;
-                dislikeCount++;
-            }
-            else if (!upvote.upvotes && upvote.downvotes) {
-                upvote.downvotes = false;
-                dislikeCount--;
-            }
-            else {
-                console.log('There was an error');
-            }
-        }
+        });
+
+        existChecker(statementExecuted);
     })
 });
+
+function existChecker(statementExecuted) {
+    if(!statementExecuted){
+        $.post(window.location.href + 'api/upvotes', function(){
+            
+        });
+    }
+    else {
+
+    }
+}
+
+function VariableDeclare(currentEl) {
+    likeSpot = currentEl.siblings('.likecount').text()
+    likeCount = parseInt(likeSpot);
+    dislikeSpot = currentEl.siblings('.dislikecount').text();
+    dislikeCount = parseInt(dislikeSpot);
+    blogId = currentEl.parents('.blog').attr('blog-id');
+}
