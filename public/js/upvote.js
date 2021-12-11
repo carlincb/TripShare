3c677206283b32410f833bee22c3040edc06c140
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-11 11:49:54 -0700

    Most of upvote logic built out.

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -1,12 +1,67 @@
 var likeBtn = $('.like-btn');
-var dislikeBtn = $('dislike-btn');
+var dislikeBtn = $('.dislike-btn');
 
 likeBtn.click(function(){
-    likeCount = parseInt($(this).siblings('.likecount'));
+    likeSpot = $(this).siblings('.likecount').text()
+    likeCount = parseInt(likeSpot);
+    dislikeSpot = $(this).siblings('.dislikecount').text();
+    dislikeCount = parseInt(dislikeSpot);
+    blogId = $(this).parents('.blog').attr('blog-id');
+
+    $.get(window.location.href + 'api/upvotes', function(data){
+        data.forEach(upvote => {
+            if (upvote.user_id == $('nav label').attr('user-id') && upvote.blog_id == blogId) {
+                statementExecuted = true;
+                if (!upvote.upvotes && !upvote.downvotes) {
+                    upvote.upvotes = true;
+                    likeCount++;
+                }
+                else if (upvote.upvotes && !upvote.downvotes) {
+                    upvote.upvotes = false;
+                    likeCount--;
+                }
+                else if (!upvote.upvotes && upvote.downvotes) {
+                    upvote.upvotes = true;
+                    upvote.downvotes = false;
+                    likeCount++;
+                    dislikeCount--;
+                }
+                else {
+                    console.log('There was an error');
+                }
+            }
+        });
+    })
 });
 
 dislikeBtn.click(function(){
-    
+    likeSpot = $(this).siblings('.likecount').text()
+    likeCount = parseInt(likeSpot);
+    dislikeSpot = $(this).siblings('.dislikecount').text();
+    dislikeCount = parseInt(dislikeSpot);
+    blogId = $(this).parents('.blog').attr('blog-id');
+
+    $.get(window.location.href + 'api/upvotes', function(data){
+        console.log(data);
+        if (upvote.user_id == $('nav label').attr('user-id') && upvote.blog_id == blogId) {
+            statementExecuted = true;
+            if (!upvote.upvotes && !upvote.downvotes) {
+                upvote.downvotes = true;
+                dislikeCount++;
+            }
+            else if (upvote.upvotes && !upvote.downvotes) {
+                upvote.upvotes = false;
+                upvote.downvotes = true;
+                likeCount--;
+                dislikeCount++;
+            }
+            else if (!upvote.upvotes && upvote.downvotes) {
+                upvote.downvotes = false;
+                dislikeCount--;
+            }
+            else {
+                console.log('There was an error');
+            }
+        }
+    })
 });
-
-
