f70abf33fd627ea670fdcddd705a4d810105097d
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-11 11:12:43 -0700

    Added user-id as an atrribute at the top of the page so the upvotes can be iterated through based on the user id.

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -2,11 +2,11 @@ var likeBtn = $('.like-btn');
 var dislikeBtn = $('dislike-btn');
 
 likeBtn.click(function(){
-
+    likeCount = parseInt($(this).siblings('.likecount'));
 });
 
 dislikeBtn.click(function(){
-
+    
 });
 
 
