f9f972d07fdd3771dc50184c18077006fc3ca1c7
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-14 13:06:19 -0700

    Handlebars now renders the page correctly for whether the user is logged in or not.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -51,22 +51,24 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const editBlog = await Blog.update({
-      ...req.body,
-      user_id: req.session.user_id,
-    }, {
-      where: {
-        id: req.params.id,
-      },
-    }
-    );
+  if(req.params.user_id == req.session.user_id){
+    try {
+        const editBlog = await Blog.update({
+          ...req.body,
+          user_id: req.session.user_id,
+        }, {
+          where: {
+            id: req.params.id,
+          },
+        }
+        );
 
-    res.status(200).json(editBlog);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+        res.status(200).json(editBlog);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+    }
+  });
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
