b8f80e4531ba68c01785fd24cc766721d2365e0e
carlincb <carlin.colleen@gmail.com>
2021-12-26 23:40:53 +0000

    Fixing broken image for old posts.

--- public/js/profile.js
+++ public/js/profile.js
@@ -41,3 +41,17 @@ document
   .querySelector('.blog-list')
   .addEventListener('click', delButtonHandler);
 
+  // var myWidget = cloudinary.createUploadWidget({
+  //   cloudName: 'my_cloud_name', 
+  //   uploadPreset: 'my_preset'}, (error, result) => { 
+  //     if (!error && result && result.event === "success") { 
+  //       console.log('Done! Here is the image info: ', result.info); 
+  //     }
+  //   }
+  // )
+  
+  // document.getElementById("upload_widget").addEventListener("click", function(event){
+  //   event.preventDefault();
+  //     myWidget.open();
+  //   }, false);
+
